b17e1a748ca5e128113f6dfd96e5b28fc3a31ad5
kevinguts21 <kevinivanr6@gmail.com>
2025-01-30 00:05:59 -0800

    before deploying

--- motoisla/asgi.py
+++ motoisla/asgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motoisla.settings')
+settings_module = (
+    "motoisla.deployment_settings"
+    if "RENDER_EXTERNAL_HOSTNAME" in os.environ
+    else "motoisla.settings"
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_asgi_application()
--- motoisla/wsgi.py
+++ motoisla/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motoisla.settings')
+
+settings_module = (
+    "motoisla.deployment_settings"
+    if "RENDER_EXTERNAL_HOSTNAME" in os.environ
+    else "motoisla.settings"
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+
 
 application = get_wsgi_application()
