d5285668886a1b92f43d958f889b1cb712a4868c
kevinguts21 <kevinivanr6@gmail.com>
2025-01-31 13:46:38 -0800

    creating superuserx2

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then 
-   python manage.py createsuperuser --no-input
+if [[ "$CREATE_SUPERUSER" == "True" ]]; then
+    python manage.py createsuperuser --no-input
 fi
 
