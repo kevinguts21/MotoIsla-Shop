bf13a280870dc372a6d922f026c0f13747a5edbf
kevinguts21 <kevinivanr6@gmail.com>
2025-01-30 23:40:48 -0800

    sett-deployx2

--- motoisla/deployment_settings.py
+++ motoisla/deployment_settings.py
@@ -13,6 +13,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
