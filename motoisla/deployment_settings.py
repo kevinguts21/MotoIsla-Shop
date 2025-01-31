84baa79e66fbd4a15fb504ce4ec7e6f4a8e7f380
kevinguts21 <kevinivanr6@gmail.com>
2025-01-30 18:50:26 -0800

    early night .env

--- motoisla/deployment_settings.py
+++ motoisla/deployment_settings.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 
 DEBUG = False
-SECRET_KEY = os.environ.get ("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -22,8 +22,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
- 'https://motoisla-reactjs.onrender.com',
- ]
+    "https://motoisla-reactjs.onrender.com",
+    "https://motoisla-shop.onrender.com",
+]
 
 STORAGE = {
     "default": {
@@ -34,10 +35,8 @@ STORAGE = {
     },
 }
 
-DATABASES={
-    
-    'default':dj_database_url.config(
-        default= os.environ['DATABASES_URL'],
-        conn_max_age=600
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ["DATABASES_URL"], conn_max_age=600
     )
 }
